3794a0ccaf9d31f5431324ac96c40917bdace0cf
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-10 21:10:20 +0900

    booksの属性にauthorを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_09_121324) do
+ActiveRecord::Schema.define(version: 2023_02_10_120610) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
